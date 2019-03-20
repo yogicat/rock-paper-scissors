2cc6f2a8727d67a235a0a6cbeff46639febd7b33
yogicat <dahe.oh@gmail.com>
2019-03-20 13:05:41 +0900

    modify script

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors
 
+live demo : https://yogicat.github.io/rock-paper-scissors/
--- script.js
+++ script.js
@@ -1,75 +1,102 @@
 const Playground = document.getElementById('Playground');
-const RockEl = document.getElementById('Rock');
-const PaperEl = document.getElementById('Paper');
-const ScissorEl = document.getElementById('Scissor');
+const RockPaperScissors = document.querySelectorAll('.RockPaperScissor')
 const Reset = document.getElementById('Reset');
 const Results = document.getElementById('Results');
-const Selections = ['Rock', 'Paper', 'Scissor'];
-const Emoji = ['✊', '🖐', '✌️']
-let playerSelection;
-let computerSelection;
+const yourScore = document.getElementById('Your-score');
+const computerScore = document.getElementById('Computer-score');
+
+var y = 0;
+var c = 0;
+
+var playerSelection;
+var computerSelection;
+
 
 function getRandomNum(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 function computerPlay() {
+  const Selections = ['Rock', 'Paper', 'Scissor'];
+  const Emoji = ['✊', '🖐', '✌️']
   const num = getRandomNum(Selections.length);
-  const comSelect = Selections[num];
 
   document.getElementById('Computer').innerText = Emoji[num];
-  return comSelect;
+  return  Selections[num];
+}
+
+
+function setScore(resultText) {
+  switch (resultText) {
+    case 'Tied!':
+      break;
+    case 'You Win!':
+      y += 1;
+      yourScore.innerText = y;
+      break;
+    case 'You Lose!':
+      c += 1;
+      computerScore.innerText = c;
+    default:
+      break;
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-  Results.innerText = `${playerSelection} vs ${computerSelection}`
-  let resultText = ';'
-  // (rock - rock, paper, scissor)
-  if (playerSelection === 'Rock') {
-    if (computerSelection === 'Scissor') {
-      return Results.innerText = 'You Win';
-    } else if (computerSelection === 'Paper') {
-      return Results.innerText = 'You Lose';
-    } else {
-      return Results.innerText = 'You Even';
-    }
-  } else if (playerSelection === 'Paper') {
-    if (computerSelection === 'Rock') {
-      return Results.innerText = 'You Win';
-    } else if (computerSelection === 'Scissor') {
-      return Results.innerText = 'You Lose';
-    } else {
-      return Results.innerText = 'You Even';
-    }
-  } else {
-    if (computerSelection === 'Paper') {
-      return Results.innerText = 'You Win';
-    } else if (computerSelection === 'Rock') {
-      return Results.innerText = 'You Lose';
-    } else {
-      return Results.innerText = 'You Even';
-    }
+  let resultText;
+  if (playerSelection === computerSelection ) {
+    resultText = 'Tied!';
   }
+  if (playerSelection === 'Rock') {
+    resultText =  (computerSelection === 'Scissor') ? 'You Win!' : 'You Lose!';
+  }
+  if (playerSelection === 'Paper') {
+    resultText =  (computerSelection === 'Rock') ? 'You Win!' : 'You Lose!';
+  }
+  if (playerSelection === 'Scissor') {
+    resultText =  (computerSelection === 'Rock') ? 'You Lose!' : 'You Win!';
+  }
+
+  setScore(resultText);
+  return resultText;
+}
+
+function hideUnselected(playerSelection) {
+  const unselected = [...RockPaperScissors].filter(el => el.dataset.selection !== playerSelection);
+  unselected.forEach( el => el.style.display = 'none')
 }
 
 function handleClick(el) {
   playerSelection = el.dataset.selection;
   computerSelection = computerPlay()
 
-  const unselected = [RockEl, PaperEl, ScissorEl].filter(el => el.dataset.selection !== playerSelection);
-  unselected.forEach( el => el.style.display = 'none')
-  playRound(playerSelection,computerSelection);
+  hideUnselected(playerSelection)
+  Results.innerText = playRound(playerSelection,computerSelection);
+
+  setTimeout(resetElements, 1000);
 }
 
-[RockEl, PaperEl, ScissorEl].forEach(selection => {
+function resetElements() {
+  RockPaperScissors.forEach( el => el.style.display = 'initial')
+  computerSelection = undefined;
+  document.getElementById('Computer').innerText = '❓';
+  Results.innerText = 'Click to choose your weapon';
+}
+
+function resetScore() {
+  y = 0;
+  c = 0;
+  yourScore.innerText = y;
+  computerScore.innerText = c;
+}
+
+RockPaperScissors.forEach(selection => {
   selection.addEventListener('click', () => {
     handleClick(selection)
   })
 })
 
 Reset.addEventListener('click', () => {
-  [RockEl, PaperEl, ScissorEl].forEach( el => el.style.display = 'initial')
-  computerSelection = undefined;
-  document.getElementById('Computer').innerText = '❓';
-  Results.innerText = 'Click to choose your weapon';
+  resetElements()
+  resetScore()
 })
